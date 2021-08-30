8c64a2d80e39e85c3b3bd364a092c695bfa8c6bf
khaled <kkaljaf@gmail.com>
2021-08-30 22:29:06 +0200

    final result

---
+++ main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container');
+const circle = document.querySelector('.circle');
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+const listItems = document.querySelectorAll('li');
+
+openBtn.addEventListener('click', () => {
+  container.classList.add('show-nav');
+  circle.classList.add('rotate');
+});
+
+closeBtn.addEventListener('click', () => {
+  container.classList.remove('show-nav');
+  circle.classList.remove('rotate');
+});
